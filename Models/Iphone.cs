615396f90bd5ec5d4c17d27f3bddca1ec89d498a
Paulo J. Zeca <paulozeca@gmail.com>
2024-05-06 21:55:23 -0300

    Desafio Concluído - POO COM C#

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone 
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Console.WriteLine($"Modelo: {modelo}, Número: {numero}, IMEI: {imei}, Total Mémoria: {memoria}");
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+          Console.WriteLine("Instalando Aplicativo... " + nomeApp);
+        }
+            
     }
 }
