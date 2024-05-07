0eae53e9777f220de2e835a05ee0055e10333910
Paulo J. Zeca <paulozeca@gmail.com>
2024-05-06 21:59:29 -0300

    Desafio Concluído - POO COM C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"(14)9989-5566",modelo:"Nokia N100", imei:"123ZYZ456ASD",memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone");
+Smartphone iphone = new Iphone(numero:"(14)9988-7788",modelo:"IPhone 14", imei:"456ASX456YTU",memoria:256);
+iphone.InstalarAplicativo("Discord");
+iphone.ReceberLigacao();
